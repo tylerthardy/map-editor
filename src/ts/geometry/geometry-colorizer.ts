c2d6f1e8ad064140040c4cb332c2839878fa9059
Tyler Hardy <tylerthardy@gmail.com>
2021-08-04 02:02:18 -0400

    Remove low level helpers

--- src/ts/geometry/geometry-colorizer.ts
+++ src/ts/geometry/geometry-colorizer.ts
@@ -1,7 +1,6 @@
 import { BufferGeometry, BufferAttribute, Color } from "three";
 import { ColorDefinition, ColorDefinitions } from "./color/color-definition";
 
-// TODO: Clean up and make tile methods based on generateColorAttribute
 export abstract class GeometryColorizer {
     static generateColorAttribute(colors: ColorDefinition[]): BufferAttribute {
         // Each vertex has 3 color values (r, g, b)
@@ -10,47 +9,18 @@ export abstract class GeometryColorizer {
         return bufferAttribute;
     }
 
-    static squareColors(geometry: BufferGeometry, colors: ColorDefinition[]) {
-        const count = geometry.attributes.position.count;
-        geometry.setAttribute('color', new BufferAttribute( new Float32Array( count * 3 ), 3 ) );
-        const colors1 = geometry.attributes.color;
-        colors.forEach((cd, i) => colors1.setXYZ(i, cd.r, cd.g, cd.b));
-
-        return geometry;
-    }
-
-    static testTriangleColors(geometry: BufferGeometry) {
-        const count = geometry.attributes.position.count;
-        geometry.setAttribute( 'color', new BufferAttribute( new Float32Array( count * 3 ), 3 ) );
-        const colors1 = geometry.attributes.color;
+    static testTriangleColorAttribute(count: number): BufferAttribute {
+        const bufferAttribute = new BufferAttribute( new Float32Array( count * 3 ), 3 );
         const color = new Color();
         color.g = 1;
         color.b = 0;
         for (let i = 0; i < count; i ++) {
             const isOdd = Math.floor(i / 3) % 2 == 0;
             color.r = isOdd ? 1 : 0;
-            colors1.setXYZ( i, color.r, color.g, color.b );
+            bufferAttribute.setXYZ( i, color.r, color.g, color.b );
         }
 
-        return geometry;
-    }
-
-    static testSquareColors(geometry: BufferGeometry): BufferGeometry {
-        const count = geometry.attributes.position.count;
-        geometry.setAttribute( 'color', new BufferAttribute( new Float32Array( count * 3 ), 3 ) );
-        const colors1 = geometry.attributes.color;
-        this.getTestSquareColors(count).forEach((cd, i) => colors1.setXYZ(i, cd.r, cd.g, cd.b));
-
-        return geometry;
-    }
-
-    static randomSquareColors(geometry: BufferGeometry): BufferGeometry {
-        const count = geometry.attributes.position.count;
-        geometry.setAttribute( 'color', new BufferAttribute( new Float32Array( count * 3 ), 3 ) );
-        const colors1 = geometry.attributes.color;
-        this.getRandomSquareColors(count).forEach((cd, i) => colors1.setXYZ(i, cd.r, cd.g, cd.b));
-
-        return geometry;
+        return bufferAttribute;
     }
 
     static getSolidSquareColor(numberOfSquares: number, color: ColorDefinition): ColorDefinition[] {
