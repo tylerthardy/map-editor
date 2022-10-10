4fd1e9c170ffcb3d6dce199c953e7b1cec9c2d6a
Tyler Hardy <tylerthardy@gmail.com>
2022-10-10 00:10:38 -0400

    Move terrain generation from chunk to terrain

--- src/app/common/services/chunk/chunk.ts
+++ src/app/common/services/chunk/chunk.ts
@@ -1,4 +1,3 @@
-import { BufferAttribute, Vector3 } from 'three';
 import { Terrain } from '../terrain';
 import { Tile } from './tile';
 
@@ -10,75 +9,6 @@ export class Chunk {
 
   constructor(tiles: Tile[]) {
     this.tiles = tiles;
-    this.terrain = this.generateTerrain();
-  }
-
-  private generateTerrain(): Terrain {
-    const verticesPerTriangle: number = 3;
-    const trianglesPerTile: number = 2;
-    const vertexCount: number = Chunk.size * Chunk.size * verticesPerTriangle * trianglesPerTile * 3;
-    const colorAttribute = new BufferAttribute(new Float32Array(vertexCount), 3);
-    const positionAttribute = new BufferAttribute(new Float32Array(vertexCount), 3);
-
-    for (var y = 0; y < Chunk.size; y++) {
-      for (var x = 0; x < Chunk.size; x++) {
-        const tileIndex = y * Chunk.size + x;
-        const tile: Tile = this.tiles[tileIndex];
-
-        let nextXTile: Tile = x + 1 >= Chunk.size ? tile : this.tiles[tileIndex + 1];
-        let nextYTile: Tile = y + 1 >= Chunk.size ? tile : this.tiles[tileIndex + Chunk.size];
-        // FIXME: Use proper logic for diagonals
-        let nextXYTile: Tile =
-          x + 1 >= Chunk.size || y + 1 >= Chunk.size ? tile : this.tiles[tileIndex + Chunk.size + 1];
-
-        const topLeftPosition = new Vector3(x, tile.cornerElevations[0], y);
-        const topLeftColor = tile.topLeftCornerColor;
-
-        const topRightPosition = new Vector3(x + Tile.size, tile.cornerElevations[1], y);
-        const topRightColor = nextXTile.topLeftCornerColor;
-
-        const bottomLeftPosition = new Vector3(x, tile.cornerElevations[2], y + Tile.size);
-        const bottomLeftColor = nextYTile.topLeftCornerColor;
-
-        const bottomRightPosition = new Vector3(x + Tile.size, tile.cornerElevations[3], y + Tile.size);
-        const bottomRightColor = nextXYTile.topLeftCornerColor;
-
-        if (topLeftPosition.distanceTo(bottomRightPosition) < bottomLeftPosition.distanceTo(topRightPosition)) {
-          // triangle 1
-          colorAttribute.setXYZ(tileIndex * 6 + 0, topLeftColor.r, topLeftColor.g, topLeftColor.b);
-          positionAttribute.setXYZ(tileIndex * 6 + 0, ...topLeftPosition.toArray());
-          colorAttribute.setXYZ(tileIndex * 6 + 1, bottomLeftColor.r, bottomLeftColor.g, bottomLeftColor.b);
-          positionAttribute.setXYZ(tileIndex * 6 + 1, ...bottomLeftPosition.toArray());
-          colorAttribute.setXYZ(tileIndex * 6 + 2, bottomRightColor.r, bottomRightColor.g, bottomRightColor.b);
-          positionAttribute.setXYZ(tileIndex * 6 + 2, ...bottomRightPosition.toArray());
-
-          // triangle 2
-          colorAttribute.setXYZ(tileIndex * 6 + 3, topLeftColor.r, topLeftColor.g, topLeftColor.b);
-          positionAttribute.setXYZ(tileIndex * 6 + 3, ...topLeftPosition.toArray());
-          colorAttribute.setXYZ(tileIndex * 6 + 4, bottomRightColor.r, bottomRightColor.g, bottomRightColor.b);
-          positionAttribute.setXYZ(tileIndex * 6 + 4, ...bottomRightPosition.toArray());
-          colorAttribute.setXYZ(tileIndex * 6 + 5, topRightColor.r, topRightColor.g, topRightColor.b);
-          positionAttribute.setXYZ(tileIndex * 6 + 5, ...topRightPosition.toArray());
-        } else {
-          // triangle 1
-          colorAttribute.setXYZ(tileIndex * 6 + 0, topLeftColor.r, topLeftColor.g, topLeftColor.b);
-          positionAttribute.setXYZ(tileIndex * 6 + 0, ...topLeftPosition.toArray());
-          colorAttribute.setXYZ(tileIndex * 6 + 1, bottomLeftColor.r, bottomLeftColor.g, bottomLeftColor.b);
-          positionAttribute.setXYZ(tileIndex * 6 + 1, ...bottomLeftPosition.toArray());
-          colorAttribute.setXYZ(tileIndex * 6 + 2, topRightColor.r, topRightColor.g, topRightColor.b);
-          positionAttribute.setXYZ(tileIndex * 6 + 2, ...topRightPosition.toArray());
-
-          // triangle 2
-          colorAttribute.setXYZ(tileIndex * 6 + 3, bottomLeftColor.r, bottomLeftColor.g, bottomLeftColor.b);
-          positionAttribute.setXYZ(tileIndex * 6 + 3, ...bottomLeftPosition.toArray());
-          colorAttribute.setXYZ(tileIndex * 6 + 4, bottomRightColor.r, bottomRightColor.g, bottomRightColor.b);
-          positionAttribute.setXYZ(tileIndex * 6 + 4, ...bottomRightPosition.toArray());
-          colorAttribute.setXYZ(tileIndex * 6 + 5, topRightColor.r, topRightColor.g, topRightColor.b);
-          positionAttribute.setXYZ(tileIndex * 6 + 5, ...topRightPosition.toArray());
-        }
-      }
-    }
-
-    return new Terrain(positionAttribute, colorAttribute);
+    this.terrain = new Terrain(this.tiles, Chunk.size);
   }
 }
--- src/app/common/services/terrain/terrain.ts
+++ src/app/common/services/terrain/terrain.ts
@@ -1,81 +1,87 @@
-import { BufferAttribute, BufferGeometry, Color, Face, Vector2 } from 'three';
+import { BufferAttribute, BufferGeometry, Color, Face, Vector2, Vector3 } from 'three';
+import { Tile } from '../chunk/tile';
 import { GeometryColorizer } from './geometry-colorizer';
 import { TerrainGenerator } from './terrain-generator';
 
 export class Terrain {
-  public HEIGHT: number = 10;
-  public WIDTH: number = 10;
   public geometry3d!: BufferGeometry;
   public geometry2d!: BufferGeometry;
 
-  private _tileColors!: Color[];
-  private _highlightedTiles: { [key: number]: { [key: number]: boolean } } = {};
+  private tileColors!: Color[];
+  private highlightedTiles: { [key: number]: { [key: number]: boolean } } = {};
 
-  private _positionAttribute!: BufferAttribute;
-  private _colorAttribute!: BufferAttribute;
+  private positionAttribute!: BufferAttribute;
+  private colorAttribute!: BufferAttribute;
+  private width: number;
+  private height: number;
 
-  constructor(positionAttribute: BufferAttribute, colorAttribute: BufferAttribute) {
-    this._tileColors = GeometryColorizer.getSolidSquareColor(this.WIDTH * this.HEIGHT, new Color('gray'));
-    this._positionAttribute = positionAttribute;
-    this._colorAttribute = colorAttribute;
+  constructor(tiles: Tile[], size: number) {
+    this.width = size;
+    this.height = size;
+
+    const attributes: { colorAttribute: BufferAttribute; positionAttribute: BufferAttribute } =
+      Terrain.generateAttributesFromTiles(tiles, size);
+    this.tileColors = GeometryColorizer.getSolidSquareColor(this.width * this.height, new Color('gray'));
+    this.positionAttribute = attributes.positionAttribute;
+    this.colorAttribute = attributes.colorAttribute;
 
     this.geometry3d = new BufferGeometry();
-    this.geometry3d.attributes.position = this._positionAttribute;
-    this.geometry3d.attributes.color = this._colorAttribute;
+    this.geometry3d.attributes.position = this.positionAttribute;
+    this.geometry3d.attributes.color = this.colorAttribute;
     this.geometry3d.computeVertexNormals();
     this.geometry3d.attributes.position.needsUpdate = true;
     this.geometry3d.attributes.color.needsUpdate = true;
 
     this.geometry2d = new BufferGeometry();
     this.geometry2d.copy(this.geometry3d);
-    this.geometry2d.attributes.position = TerrainGenerator.flattenVertices(this._positionAttribute);
-    this.geometry2d.attributes.color = this._colorAttribute;
+    this.geometry2d.attributes.position = TerrainGenerator.flattenVertices(this.positionAttribute);
+    this.geometry2d.attributes.color = this.colorAttribute;
     this.geometry3d.attributes.position.needsUpdate = true;
     this.geometry3d.attributes.color.needsUpdate = true;
   }
 
   public getFaceXY(face: Face): Vector2 {
     const tileIndex = Math.floor(face.a / 6);
-    const x = tileIndex % this.WIDTH;
-    const y = Math.floor(tileIndex / this.WIDTH);
+    const x = tileIndex % this.width;
+    const y = Math.floor(tileIndex / this.width);
     return new Vector2(x, y);
   }
 
   public highlightTile(x: number, y: number, color: Color) {
     this.validateValidCoordinates(x, y);
 
-    if (this._highlightedTiles[x] && this._highlightedTiles[x][y]) {
+    if (this.highlightedTiles[x] && this.highlightedTiles[x][y]) {
       return;
     }
 
     this.unhighlightTiles();
-    this._highlightedTiles[x] = this._highlightedTiles[x] ?? {};
-    this._highlightedTiles[x][y] = true;
+    this.highlightedTiles[x] = this.highlightedTiles[x] ?? {};
+    this.highlightedTiles[x][y] = true;
     this.setTileAttributeColor(x, y, color);
   }
 
   public unhighlightTiles() {
-    Object.entries(this._highlightedTiles).forEach((xValue: [string, { [key: number]: boolean }]) => {
+    Object.entries(this.highlightedTiles).forEach((xValue: [string, { [key: number]: boolean }]) => {
       const x: number = Number.parseInt(xValue[0]);
       Object.entries(xValue[1]).forEach((yValue: [string, boolean]) => {
         const y = Number.parseInt(yValue[0]);
-        const tileIndex = y * this.WIDTH + x;
+        const tileIndex = y * this.width + x;
         const vertexIndex = tileIndex * 3 * 2;
-        const originalColor = this._tileColors[vertexIndex];
+        const originalColor = this.tileColors[vertexIndex];
         this.setTileAttributeColor(x, y, originalColor);
       });
     });
-    this._highlightedTiles = {};
+    this.highlightedTiles = {};
   }
 
   public setTileColor(x: number, y: number, color: Color): void {
     this.validateValidCoordinates(x, y);
 
-    const tileIndex = y * this.WIDTH + x;
+    const tileIndex = y * this.width + x;
     const vertexIndex = tileIndex * 3 * 2;
 
     // Set the data
-    this._tileColors[vertexIndex] = color;
+    this.tileColors[vertexIndex] = color;
 
     this.setTileAttributeColor(x, y, color);
   }
@@ -87,24 +93,98 @@ export class Terrain {
   public setTileAttributeColor(x: number, y: number, color: Color): void {
     this.validateValidCoordinates(x, y);
 
-    const tileIndex = y * this.WIDTH + x;
+    const tileIndex = y * this.width + x;
     const vertexIndex = tileIndex * 3 * 2;
 
     for (let index = vertexIndex; index < vertexIndex + 6; index++) {
-      this._colorAttribute.setXYZ(index, color.r, color.g, color.b);
+      this.colorAttribute.setXYZ(index, color.r, color.g, color.b);
     }
-    this._colorAttribute.needsUpdate = true;
+    this.colorAttribute.needsUpdate = true;
   }
 
   public getTileColor(x: number, y: number): Color {
     this.validateValidCoordinates(x, y);
 
-    const tileIndex = y * this.WIDTH + x;
+    const tileIndex = y * this.width + x;
     const vertex = tileIndex * 3 * 2;
-    return this._tileColors[vertex];
+    return this.tileColors[vertex];
+  }
+
+  private static generateAttributesFromTiles(
+    tiles: Tile[],
+    size: number
+  ): { colorAttribute: BufferAttribute; positionAttribute: BufferAttribute } {
+    const verticesPerTriangle: number = 3;
+    const trianglesPerTile: number = 2;
+    const vertexCount: number = size * size * verticesPerTriangle * trianglesPerTile * 3;
+    const colorAttribute = new BufferAttribute(new Float32Array(vertexCount), 3);
+    const positionAttribute = new BufferAttribute(new Float32Array(vertexCount), 3);
+
+    for (var y = 0; y < size; y++) {
+      for (var x = 0; x < size; x++) {
+        const tileIndex = y * size + x;
+        const tile: Tile = tiles[tileIndex];
+
+        let nextXTile: Tile = x + 1 >= size ? tile : tiles[tileIndex + 1];
+        let nextYTile: Tile = y + 1 >= size ? tile : tiles[tileIndex + size];
+        // FIXME: Use proper logic for diagonals
+        let nextXYTile: Tile = x + 1 >= size || y + 1 >= size ? tile : tiles[tileIndex + size + 1];
+
+        const topLeftPosition = new Vector3(x, tile.cornerElevations[0], y);
+        const topLeftColor = tile.topLeftCornerColor;
+
+        const topRightPosition = new Vector3(x + Tile.size, tile.cornerElevations[1], y);
+        const topRightColor = nextXTile.topLeftCornerColor;
+
+        const bottomLeftPosition = new Vector3(x, tile.cornerElevations[2], y + Tile.size);
+        const bottomLeftColor = nextYTile.topLeftCornerColor;
+
+        const bottomRightPosition = new Vector3(x + Tile.size, tile.cornerElevations[3], y + Tile.size);
+        const bottomRightColor = nextXYTile.topLeftCornerColor;
+
+        if (topLeftPosition.distanceTo(bottomRightPosition) < bottomLeftPosition.distanceTo(topRightPosition)) {
+          // triangle 1
+          colorAttribute.setXYZ(tileIndex * 6 + 0, topLeftColor.r, topLeftColor.g, topLeftColor.b);
+          positionAttribute.setXYZ(tileIndex * 6 + 0, ...topLeftPosition.toArray());
+          colorAttribute.setXYZ(tileIndex * 6 + 1, bottomLeftColor.r, bottomLeftColor.g, bottomLeftColor.b);
+          positionAttribute.setXYZ(tileIndex * 6 + 1, ...bottomLeftPosition.toArray());
+          colorAttribute.setXYZ(tileIndex * 6 + 2, bottomRightColor.r, bottomRightColor.g, bottomRightColor.b);
+          positionAttribute.setXYZ(tileIndex * 6 + 2, ...bottomRightPosition.toArray());
+
+          // triangle 2
+          colorAttribute.setXYZ(tileIndex * 6 + 3, topLeftColor.r, topLeftColor.g, topLeftColor.b);
+          positionAttribute.setXYZ(tileIndex * 6 + 3, ...topLeftPosition.toArray());
+          colorAttribute.setXYZ(tileIndex * 6 + 4, bottomRightColor.r, bottomRightColor.g, bottomRightColor.b);
+          positionAttribute.setXYZ(tileIndex * 6 + 4, ...bottomRightPosition.toArray());
+          colorAttribute.setXYZ(tileIndex * 6 + 5, topRightColor.r, topRightColor.g, topRightColor.b);
+          positionAttribute.setXYZ(tileIndex * 6 + 5, ...topRightPosition.toArray());
+        } else {
+          // triangle 1
+          colorAttribute.setXYZ(tileIndex * 6 + 0, topLeftColor.r, topLeftColor.g, topLeftColor.b);
+          positionAttribute.setXYZ(tileIndex * 6 + 0, ...topLeftPosition.toArray());
+          colorAttribute.setXYZ(tileIndex * 6 + 1, bottomLeftColor.r, bottomLeftColor.g, bottomLeftColor.b);
+          positionAttribute.setXYZ(tileIndex * 6 + 1, ...bottomLeftPosition.toArray());
+          colorAttribute.setXYZ(tileIndex * 6 + 2, topRightColor.r, topRightColor.g, topRightColor.b);
+          positionAttribute.setXYZ(tileIndex * 6 + 2, ...topRightPosition.toArray());
+
+          // triangle 2
+          colorAttribute.setXYZ(tileIndex * 6 + 3, bottomLeftColor.r, bottomLeftColor.g, bottomLeftColor.b);
+          positionAttribute.setXYZ(tileIndex * 6 + 3, ...bottomLeftPosition.toArray());
+          colorAttribute.setXYZ(tileIndex * 6 + 4, bottomRightColor.r, bottomRightColor.g, bottomRightColor.b);
+          positionAttribute.setXYZ(tileIndex * 6 + 4, ...bottomRightPosition.toArray());
+          colorAttribute.setXYZ(tileIndex * 6 + 5, topRightColor.r, topRightColor.g, topRightColor.b);
+          positionAttribute.setXYZ(tileIndex * 6 + 5, ...topRightPosition.toArray());
+        }
+      }
+    }
+
+    return {
+      colorAttribute,
+      positionAttribute
+    };
   }
 
   private validateValidCoordinates(x: number, y: number): void {
-    if (x >= this.WIDTH || y >= this.HEIGHT || x < 0 || y < 0) throw new Error('Invalid dimension');
+    if (x >= this.width || y >= this.height || x < 0 || y < 0) throw new Error('Invalid dimension');
   }
 }
